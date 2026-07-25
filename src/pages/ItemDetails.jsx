178d171c52ef937897b1f3a3cbf73ca94f2c402f
MdIslamNeon <2003raisul@gmail.com>
2026-07-25 12:27:29 -0700

    Added loading state

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,20 +1,19 @@
 import React, { useEffect, useState } from "react";
-import EthImage from "../images/ethereum.svg";
-import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
-import nftImage from "../images/nftImage.jpg";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import ItemDetail from "../components/ItemDetail";
+import ItemDetailLoading from "../components/ItemDetailLoading";
 
 const ItemDetails = () => {
-
-  const {id} = useParams();
-  const [itemDetails, setItemDetails] = useState({})
-  const [loading, setLoading] = useState(true)
+  const { id } = useParams();
+  const [itemDetails, setItemDetails] = useState({});
+  const [loading, setLoading] = useState(true);
 
   async function fetchItemDetails() {
     try {
-      const {data} = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=${id}`);
+      const { data } = await axios.get(
+        `https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=${id}`,
+      );
       setItemDetails(data);
     } catch (error) {
       console.log(error);
@@ -37,72 +36,11 @@ const ItemDetails = () => {
         <div id="top"></div>
         <section aria-label="section" className="mt90 sm-mt-0">
           <div className="container">
-            <div className="row">
-              <div className="col-md-6 text-center">
-                <img
-                  src={itemDetails.nftImage}
-                  className="img-fluid img-rounded mb-sm-30 nft-image"
-                  alt=""
-                />
-              </div>
-              <div className="col-md-6">
-                <div className="item_info">
-                  <h2>{itemDetails.title}</h2>
-                  <div className="item_info_counts">
-                    <div className="item_info_views">
-                      <i className="fa fa-eye"></i>
-                      {itemDetails.views}
-                    </div>
-                    <div className="item_info_like">
-                      <i className="fa fa-heart"></i>
-                      {itemDetails.likes}
-                    </div>
-                  </div>
-                  <p>
-                    {itemDetails.description}
-                  </p>
-                  <div className="d-flex flex-row">
-                    <div className="mr40">
-                      <h6>Owner</h6>
-                      <div className="item_author">
-                        <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={itemDetails.ownerImage} alt="" />
-                            <i className="fa fa-check"></i>
-                          </Link>
-                        </div>
-                        <div className="author_list_info">
-                          <Link to="/author">{itemDetails.ownerName}</Link>
-                        </div>
-                      </div>
-                    </div>
-                    <div></div>
-                  </div>
-                  <div className="de_tab tab_simple">
-                    <div className="de_tab_content">
-                      <h6>Creator</h6>
-                      <div className="item_author">
-                        <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={itemDetails.creatorImage} alt="" />
-                            <i className="fa fa-check"></i>
-                          </Link>
-                        </div>
-                        <div className="author_list_info">
-                          <Link to="/author">{itemDetails.creatorName}</Link>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="spacer-40"></div>
-                    <h6>Price</h6>
-                    <div className="nft-item-price">
-                      <img src={EthImage} alt="" />
-                      <span>{itemDetails.price}</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            {loading ? (
+              <ItemDetailLoading />
+            ) : (
+              <ItemDetail itemDetails={itemDetails} />
+            )}
           </div>
         </section>
       </div>
