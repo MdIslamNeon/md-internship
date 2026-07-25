ee06e2137fd8a67c716d3a6635525482c3994228
MdIslamNeon <2003raisul@gmail.com>
2026-07-25 12:53:56 -0700

    testing branch

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -4,6 +4,7 @@ import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
 
 const NewItems = () => {
+  // testing git
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
