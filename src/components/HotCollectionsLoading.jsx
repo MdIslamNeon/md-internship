47e64232977ecd8d3cb044ab1807ad96ef314c06
MdIslamNeon <2003raisul@gmail.com>
2026-07-25 10:27:10 -0700

    removed unused imports

--- src/components/HotCollectionsLoading.jsx
+++ src/components/HotCollectionsLoading.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Skeleton from "./UI/Skeleton";
-import { Link } from "react-router-dom";
 
 function HotCollectionsLoading() {
   return (
--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import axios from "axios";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Skeleton from "../UI/Skeleton";
 import HotCollection from "../HotCollection.jsx";
 import HotCollectionsLoading from "../HotCollectionsLoading.jsx";
 
